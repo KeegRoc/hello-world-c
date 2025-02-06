2bf191daec20e13ca5102de91567987470dbda8d
KeegRoc <e1995478@cmaisonneuve.qc.ca>
2025-02-06 18:12:15 +0000

    fichier 1, Exercice3.2

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>  // directive pour inclure la bibliotheque standard
 
 // Declaration d'une fonction supplementaire
-int afficherDate(void){
+/*int afficherDate(void){
     printf("6 février 2025\n");    
-}
+}*/
+
+char nom[] = "Étudiant";
 
 // Fonction principale
 int main() {
     // Affiche la date
-    afficherDate();
+    printf("Bonjour, %s !\n",nom);
     return 0;   // Fin normale du programme
 }
