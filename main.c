fdb38da7c8f38a7774cba085e09b2e5902415b06
KeegRoc <e1995478@cmaisonneuve.qc.ca>
2025-02-13 02:39:51 +0000

    fichier activité 5.2.2

--- main.c
+++ main.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char operateur = '/';
-int a = 10;
-int b = 5;
-
 int main()
 {
-   switch (operateur){
-        case '+':
-        printf("%d + %d = %d\n", a, b, a + b);
-            break;
-        case '-':
-        printf("%d - %d = %d\n", a, b, a - b);
-            break;
-        case '*':
-        printf("%d * %d = %d\n", a, b, a * b);
-            break;
-        case '/':
-            if (b != 0){
-            printf("%d / %d = %d\n", a, b, a / b);
-        }else {
-            printf("Division interdite!\n");
-        }
-            break;
-        default:
-            printf("Jour invalide\n"); 
+   int nombre = 0;
+   printf("Entrer un nombre supérieur à 10 :");
+   scanf("%d", &nombre);
+
+   while (nombre <= 10){
+    printf("Essayer encore : ");
+    scanf("%d", &nombre);
    }
+   printf("Bravo, vous avez entré : %d\n", nombre);
     return 0;
 }
