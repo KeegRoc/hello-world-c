f1fb26e47e7e2bfe698f93a1bc44209ea0157a01
KeegRoc <e1995478@cmaisonneuve.qc.ca>
2025-02-13 02:01:49 +0000

    Fichier d'activité 4.2.4 switch case

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int age = 3050;
+int jour = 3050;
 
 int main()
 {
-   if (age < 13){                                                     
-    printf("Enfant\n");
-   } else if (age < 18){
-    printf("Adolescent\n");
-   } else {
-    printf("Adult\n");
+   switch (jour){
+        case 1:
+            printf("Lundi\n");
+            break;
+        case 2:
+            printf("Mardi\n");
+            break;
+        case 3:
+            printf("Mercredi\n");
+            break;
+        default:
+            printf("Jour invalide\n"); 
    }
     return 0;
 }
